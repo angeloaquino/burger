87d8b306ff494ca9b3d322e2415e9f38b71a248f
Angelo Aquino <aaquino909@gmail.com>
2018-12-15 13:36:04 -0800

    initial cat

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('santa fe chicken sandwich');
+INSERT INTO burgers (burger_name) VALUES ('veggie swiss mushroom burger');
+INSERT INTO burgers (burger_name) VALUES ('belcampo classic burger');
