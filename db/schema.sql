8dd7158f363d2d3a466f37a8aca36f9a04183322
Angelo Aquino <aaquino909@gmail.com>
2018-12-15 18:49:44 -0800

    fixed orm

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,15 @@
-### Schema
+-- Drops and recreates burgers_db
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
+
+-- Use burgers_db
 USE burgers_db;
 
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
-	devoured BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+-- Creates the table "burgers"
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  burger_name VARCHAR(255),
+  devoured BOOLEAN,
+  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL 
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,10 @@
-INSERT INTO burgers (burger_name) VALUES ('santa fe chicken sandwich');
-INSERT INTO burgers (burger_name) VALUES ('veggie swiss mushroom burger');
-INSERT INTO burgers (burger_name) VALUES ('belcampo classic burger');
+USE burgers_db;
+
+INSERT INTO burgers (burger_name,devoured)
+VALUES ("Chicken Sandwich", TRUE);
+
+INSERT INTO burgers (burger_name,devoured)
+VALUES ("Howlin' Ray Chicken Sando", TRUE);
+
+INSERT INTO burgers (burger_name,devoured)
+VALUES ("Double Western Bacon Cheeseburger", FALSE);
