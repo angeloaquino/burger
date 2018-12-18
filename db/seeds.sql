2af47efd6f7cc5fb50cae12f3013ec047f2e0846
Angelo Aquino <aaquino909@gmail.com>
2018-12-17 23:17:36 -0800

    final

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,10 @@
 USE burgers_db;
 
 INSERT INTO burgers (burger_name,devoured)
-VALUES ("Chicken Sandwich", TRUE);
+VALUES ("Double Meat", TRUE);
 
 INSERT INTO burgers (burger_name,devoured)
-VALUES ("Howlin' Ray Chicken Sando", TRUE);
+VALUES ("3X3", TRUE);
 
 INSERT INTO burgers (burger_name,devoured)
-VALUES ("Double Western Bacon Cheeseburger", FALSE);
+VALUES ("Grilled Cheese", FALSE);
